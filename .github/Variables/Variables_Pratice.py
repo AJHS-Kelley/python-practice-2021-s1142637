83ccd1479640b29e0420e7df4bbecd4cdb982caf
Jordan P <90405992+s1142637@users.noreply.github.com>
2021-10-28 12:05:43 -0400

    [7B] v0.1
    added some of the most common code

--- .github/Variables/Variables_Pratice.py
+++ .github/Variables/Variables_Pratice.py
@@ -1,1 +1,11 @@
 # Variable pratice, Ryan Kelley, 10/28/21 11:40am, version 0.0
+
+# Variable Types -- Most Common
+
+my_float= -1.98654
+my_int= 10899643
+my_string= "I like turtles." 
+
+print(my_float)
+print(my_int)
+print(my_string)
